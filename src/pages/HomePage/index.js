f646b5cae12ea27fc3721e9cdf17ba4f260d9faf
寫詩羊 <Habuche@gmail.com>
2020-12-16 00:11:54 +0800

    feat(HomePage): join room

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,15 +1,20 @@
-import {
-  useHistory,
-} from "react-router-dom";
+import { useRef } from "react";
+import { useHistory } from "react-router-dom";
 
 import { getRandomChar } from "utils";
 
 export function HomePage() {
   const history = useHistory();
+  const roomDOM = useRef();
 
-  function handleClick() {
+  const handleClick = () => {
     history.replace(`/${getRandomChar(4)}/join`);
-  }
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    history.replace(`/${roomDOM.current.value}/join`);
+  };
 
   return (
     <div>
@@ -19,7 +24,15 @@ export function HomePage() {
         should generate random string
       </div>
       <h3>or</h3>
-      <input type="text" placeholder="input room id" />
+      <form onSubmit={handleSubmit}>
+        <input
+          required
+          type="text"
+          ref={roomDOM}
+          maxLength="4"
+          pattern="[0-9A-Z]{4}"
+          placeholder="input room id" />
+      </form>
     </div>
   );
 }
